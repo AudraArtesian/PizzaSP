41129f2c4077c3f4c001a444e2f823787e13cfa4
Lesya <kashinaolesya@gmail.com>
2020-06-22 18:28:27 +0300

    добавление перечисления "Инридиенты"

---
+++ src/main/java/com/epam/pizza/model/products/Ingredients.java
@@ -0,0 +1,17 @@
+package com.epam.pizza.model.products;
+
+public enum Ingredients {
+    CHIK,
+    HAM,
+    PEPPERONI,
+    TOMATO_SAUCE,
+    SPICY_CHORIZO,
+    MOZAREALLA,
+    CHEDDAR,
+    PARMESAN,
+    BLUE_CHEASE,
+    CREAM_SAUCE,
+    BERRIES,
+    CONDENSED_MILK,
+    MUSHROOMS
+}
---
+++ src/main/java/com/epam/pizza/model/products/Pizza.java
@@ -0,0 +1,19 @@
+package com.epam.pizza.model.products;
+
+public class Pizza {
+    private String pizza_type;
+    private String nutritional_value;
+    private int cCal;
+
+    public String getPizza_type() { //getter
+        return pizza_type;
+    }
+
+    public String getNutritional_value() { //getter
+        return nutritional_value;
+    }
+
+    public int getcCal() { //getter
+        return cCal;
+    }
+}
